4022f637788189277339c1e6d837807675e36aa3
Juan David Ramos <juandadelacruz2004@hotmail.com>
2023-01-23 04:09:46 -0500

    Creacion  de ventanas + modificaciones

---
+++ src/juanramos/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juanramos;
+import juanramos.ventanas.ventanaPrincipal;
+
+/**
+ *
+ * @author juand
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Prueba");
+        ventanaPrincipal miVentana = new ventanaPrincipal();
+        miVentana.setVisible(true);
+    }
+    
+}
